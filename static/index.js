abf137673c431d26298c1f54c26b782701f438ff
Gaurav Kabra <gauravkab07@gmail.com>
2022-08-13 02:49:25 +0530

    Added basic how it works section

--- static/index.js
+++ static/index.js
@@ -1,6 +1,14 @@
 let overlay = document.getElementById('overlay');
 let overlayButton = document.getElementById('navbar-overlay-button');
 
+function openOverlay() {
+    overlay.style.transform = "translateX(0)";
+}
+
+function closeOverlay() {
+    overlay.style.transform = "translateX(100%)";
+}
+
 overlayButton.addEventListener('click', function() {
     if (overlay.style.transform == "translateX(0px)"){
         closeOverlay();
@@ -10,10 +18,26 @@ overlayButton.addEventListener('click', function() {
     }
 })
 
-function openOverlay() {
-    overlay.style.transform = "translateX(0)";
-}
+const coinObserver1 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('pop-out-coin1');
+        }
+    });
+});
 
-function closeOverlay() {
-    overlay.style.transform = "translateX(100%)";
-}
+const coinObserver2 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('pop-out-coin2');
+        }
+    });
+});
+
+document.querySelectorAll('.how-it-works-coin-type1').forEach(coin => {
+    coinObserver1.observe(coin);
+});
+
+document.querySelectorAll('.how-it-works-coin-type2').forEach(coin => {
+    coinObserver2.observe(coin);
+})
